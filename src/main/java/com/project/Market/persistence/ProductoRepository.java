51c2bd139e330f2baafbb8400d87e562287d7901
santiagopereira12 <spereira98@misena.edu.co>
2024-01-30 10:21:33 -0500

    Implementación de la anotación @Repository

--- src/main/java/com/project/Market/persistence/ProductoRepository.java
+++ src/main/java/com/project/Market/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.project.Market.persistence;
 
 import com.project.Market.persistence.crud.ProductoCrudRepository;
 import com.project.Market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
     public List<Producto> getAll(){
@@ -18,5 +20,17 @@ public class ProductoRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 }
